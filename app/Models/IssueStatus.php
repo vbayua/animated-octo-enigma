4167b21234e1add7595452d4aa4530feb21685fc
Viskal <viskal.bayu@gmail.com>
2024-02-20 14:45:32 +0700

    Fix the relationship in the correct logic

--- app/Models/IssueStatus.php
+++ app/Models/IssueStatus.php
@@ -4,14 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class IssueStatus extends Model
 {
     use HasFactory;
 
-    public function issue(): BelongsTo
+    public function issue(): HasMany
     {
-        return $this->belongsTo(Issue::class);
+        return $this->hasMany(Issue::class);
     }
 }
